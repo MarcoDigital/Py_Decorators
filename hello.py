1b3e04215e5b2275149cfb0238f8bae1687e62ac
Marco <marcorem@live.nl>
2021-03-15 13:36:26 +0100

    Made first decorator

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from time import time
+
+
+def mijntimer(func):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        t3 = round(t2-t1, 3)
+        print(f"Het duurde {t3} seconden.")
+        return result
+    wrapper()
+
+
+@mijntimer
+def mijnfunctie():
+    for i in range(10000000):
+        print(i*i)
